cb6666f142775c3affc120e4d8f4e8caaea601ea
ajwensley <88682963+ajwensley@users.noreply.github.com>
2022-01-24 14:18:07 -0500

    Create validPin.java

---
+++ validPin.java
@@ -0,0 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class validPin {
+
+    public static void main(String[] args) {
+        System.out.println(validate("121317"));
+        System.out.println(validate("1234"));
+        System.out.println(validate("45135"));
+        System.out.println(validate("89abc1"));
+        System.out.println(validate("900876"));
+        System.out.println(validate(" 4983"));
+    }
+
+    public static boolean validate(String input) {
+        Pattern pattern = Pattern.compile("^\\d{4}$|^\\d{6}$");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+}
